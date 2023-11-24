3d9b1f272dd4d785d488eb6b75ec29cd39a417ef
Sle0999 <sle0999@gmail.com>
2023-11-23 19:11:39 -0600

    Update spotify.py

--- plexist/modules/spotify.py
+++ plexist/modules/spotify.py
@@ -9,7 +9,7 @@ from .plex import update_or_create_plex_playlist
 
 
 def _get_sp_user_playlists(
-    sp: spotipy.Spotify, user_id: str, suffix: str = " - " + user_inputs.spotify_playlist_suffix
+    sp: spotipy.Spotify, user_id: str, suffix: str = " - " + UserInputs.spotify_playlist_suffix
 ) -> List[Playlist]:
     playlists = []
 
@@ -20,7 +20,7 @@ def _get_sp_user_playlists(
                 playlists.append(
                     Playlist(
                         id=playlist["uri"],
-                        name=playlist["name"] + " - " + user_inputs.spotify_playlist_suffix,
+                        name=playlist["name"] + " - " + UserInputs.spotify_playlist_suffix,
                         description=playlist.get("description", ""),
                         poster=""
                         if len(playlist["images"]) == 0
